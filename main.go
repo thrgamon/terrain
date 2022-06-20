0497f7154980f14310be0f21b5fa6e484a13f7a4
Tom Gamon <tom@tomgamon.com>
2022-06-20 16:36:43 +1000

    Smooth out the slopes

--- main.go
+++ main.go
@@ -1,56 +1,122 @@
 package main
 
 import (
+	"html/template"
 	"math/rand"
+	"net/http"
 	"time"
-  "net/http"
-  "html/template"
 )
 
-func main()  {
+type Earth struct {
+	depth int
+	left  *Earth
+	right *Earth
+}
 
-  http.HandleFunc("/", chartHandler)
+type Terrain struct {
+	width     int
+	depth     int
+	heightmap []*Earth
+	seed      rand.Source
+}
+
+func newTerrain(depth int, width int, seed rand.Source) *Terrain {
+	terrain := Terrain{depth: depth, width: width, seed: seed}
+
+	for i := 0; i < width; i++ {
+		e := &Earth{depth: depth}
+		terrain.heightmap = append(terrain.heightmap, e)
+	}
+
+	for i := 0; i < width; i++ {
+		e := terrain.heightmap[i]
+		if i-1 > 0 {
+			e.left = terrain.heightmap[i-1]
+		}
+		if i+1 < width {
+			e.right = terrain.heightmap[i+1]
+		}
+	}
+
+	return &terrain
+}
+
+func (terr *Terrain) toSlice() (slizzard []int) {
+	for _, earth := range terr.heightmap {
+		slizzard = append(slizzard, earth.depth)
+	}
+	return slizzard
+}
+
+func main() {
+	http.HandleFunc("/", chartHandler)
 	http.ListenAndServe(":8081", nil)
 }
 
 func chartHandler(w http.ResponseWriter, _ *http.Request) {
-  seed := rand.NewSource(time.Now().UnixNano())
-  depth := 100
-  width := 1000
-  rain := 10000
-  data := Foo(seed, depth, width, rain)
+	seed := rand.NewSource(time.Now().UnixNano())
+	terr := newTerrain(100, 1000, seed)
+	rain := 10000
+	data := Foo(terr, rain)
 
-  err := renderTemplate(w, data)
+	err := renderTemplate(w, data.toSlice())
 
-  if err != nil {
-    handleError(w, err)
-  }
+	if err != nil {
+		handleError(w, err)
+	}
 }
 
-
 func handleError(w http.ResponseWriter, err error) {
-  http.Error(w, err.Error(), http.StatusInternalServerError)
+	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
 
 func renderTemplate(w http.ResponseWriter, data []int) error {
-  var t = template.Must(template.ParseFiles("page.html"))
-  err := t.Execute(w, data)
+	var t = template.Must(template.ParseFiles("page.html"))
+	err := t.Execute(w, data)
 
-  return err
+	return err
 }
 
-func Foo(seed rand.Source, depth int, width int, rain int) []int{
-  rng := rand.New(seed)
+func Foo(terr *Terrain, rain int) *Terrain {
+	rng := rand.New(terr.seed)
 
-  line := []int{}
-  for i := 0; i < width; i++ {
-   line = append(line, depth) 
-  }
-  for i := 0; i < rain; i++ {
-    spot := rng.Intn(width)
-    println(spot)
-    line[spot] = line[spot] - 1
-  }
+	for i := 0; i < rain; i++ {
+		spot := rng.Intn(terr.width)
+		trickleDown := false
+		e := terr.heightmap[spot]
+		if e.right != nil && e.right.depth < e.depth {
+			e.right.depth = e.right.depth - 1
+			trickleDown = true
+		}
 
-  return line
+		if e.left != nil && e.left.depth < e.depth {
+			e.left.depth = e.left.depth - 1
+			trickleDown = true
+		}
+
+		if !trickleDown {
+			e.depth = e.depth - 1
+		}
+	}
+
+
+	for _, earth := range terr.heightmap {
+  	slopify(earth)
+	}
+
+	return terr
+}
+
+func slopify(e *Earth) {
+	if e.right != nil && e.right.depth-e.depth > 1 {
+		e.right.depth = e.depth + 1
+		slopify(e.right)
+	}
+
+	if e.left != nil && e.left.depth-e.depth > 1 {
+		e.left.depth = e.depth + 1
+		slopify(e.left)
+	}
+
+	return
 }
--- main_test.go
+++ main_test.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"math/rand"
+	"reflect"
 	"testing"
-  "reflect"
 )
+
 var testSeed = rand.NewSource(125)
 
 func TestFoo(t *testing.T) {
-  line := Foo(testSeed, 10, 10, 10)
+	terr := newTerrain(10, 10, testSeed)
+	Foo(terr, 10)
 
-	got := line
-	want := []int{10, 9, 8, 10, 10, 10, 8, 9, 7, 9}
+	got := terr.toSlice()
+	want := []int{8, 7, 8, 9, 9, 8, 7, 6, 5, 6}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
